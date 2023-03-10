01e14bffb90ab65e460ac70f27cb13773c7bda08
quiz3 <113859565+quiz3@users.noreply.github.com>
2023-03-10 01:34:26 -0500

    more testing

--- test_graph.py
+++ test_graph.py
@@ -1,6 +1,9 @@
 from graph import TransitSystem, _Station
 
+# Singapore test
 singapore = TransitSystem()
 singapore.load_from_json("dataset/dataset/singapore.json")
 singapore.temporary_render(show_name=True)
 
+# Toronto test
+...
