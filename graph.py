8259fdc6ea43e742fc61b3ae14aae7a9e1b7ca45
gursi26 <gursimar.batra@gmail.com>
2023-04-02 01:26:54 -0400

    removed comments

--- graph.py
+++ graph.py
@@ -305,13 +305,6 @@ class TransitSystem:
 
         curr_station = dest_station
 
-        # while curr_station != start_station:
-        #     path.insert(0, curr_station)
-        #     total_path_distance += self.get_edge_length(curr_station, track_prev_station[curr_station])
-        #     curr_station = track_prev_station[curr_station]
-        #
-        # path.insert(0, start_station)
-
         while curr_station != start_station:
             path.append(curr_station)
             total_path_distance += self.get_edge_length(curr_station, track_prev_station[curr_station])
